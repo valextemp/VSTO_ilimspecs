bd6b8cffb7e6144ade5601489ce1f5f65ae83fd8
Alex Remote <valexwork@mail.ru>
2018-09-10 15:36:17 +0300

    final release

--- ilimspecs.BL/StringExtensions.cs
+++ ilimspecs.BL/StringExtensions.cs
@@ -9,6 +9,7 @@ namespace ilimspecs.BL
     public static class StringExtensions
     {
         //Расширяющий метод для сравнения строк без учета регистра
+        //
         public static bool Contains(this String str, String substring,
                                     StringComparison comp)
         {
